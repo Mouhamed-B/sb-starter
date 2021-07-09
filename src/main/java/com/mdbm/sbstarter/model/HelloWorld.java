453959846b35557da641ff57676254ff34fd7aa2
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2021-07-09 17:30:53 +0200

    add HelloWorld model

---
+++ src/main/java/com/mdbm/sbstarter/model/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.mdbm.sbstarter.model;
+
+
+public class HelloWorld{
+
+    private String value = "Hello World";
+
+    public HelloWorld(){
+
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
